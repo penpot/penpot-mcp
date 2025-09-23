3d29e42251e7f98285a11866640dcf49a634545e
Dominik Jain <dominik.jain@outlook.com>
2025-09-23 13:55:40 +0200

    Improve prompts

--- mcp-server/src/tools/ExecuteCodeTool.ts
+++ mcp-server/src/tools/ExecuteCodeTool.ts
@@ -45,7 +45,10 @@ export class ExecuteCodeTool extends Tool<ExecuteCodeArgs> {
             "data can be stored, simply by adding a new attribute; stored attributes can be referenced in future calls " +
             "to this tool, so any intermediate results that could come in handy later should be stored in `storage` " +
             "instead of just a fleeting variable).\n" +
-            "The tool call returns the value of the concluding return statement, if any."
+            "The tool call returns the value of the concluding return statement, if any.\n" +
+            "Note that using console.log() in your code makes no sense as you will not see the output.\n" +
+            "In general, try a simple approach first, and only if it fails, try more complex code that involves " +
+            "handling different cases (in particular error cases)."
         );
     }
 
