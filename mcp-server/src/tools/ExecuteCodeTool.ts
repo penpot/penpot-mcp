5ab14ffb9e6ff5b11cfa99736f6e217bc5c7f26c
Dominik Jain <dominik.jain@outlook.com>
2025-09-23 14:17:46 +0200

    Allow code execution to use the console, returning the full log

--- mcp-server/src/tools/ExecuteCodeTool.ts
+++ mcp-server/src/tools/ExecuteCodeTool.ts
@@ -45,8 +45,9 @@ export class ExecuteCodeTool extends Tool<ExecuteCodeArgs> {
             "data can be stored, simply by adding a new attribute; stored attributes can be referenced in future calls " +
             "to this tool, so any intermediate results that could come in handy later should be stored in `storage` " +
             "instead of just a fleeting variable).\n" +
-            "The tool call returns the value of the concluding return statement, if any.\n" +
-            "Note that using console.log() in your code makes no sense as you will not see the output.\n" +
+            "The tool call returns the value of the concluding `return` statement, if any.\n" +
+            "Any output that you generate via the `console` object will be returned to you; so you may use this" +
+            "to track what your code is doing, but you should only do so only if there is an actual need!.\n" +
             "In general, try a simple approach first, and only if it fails, try more complex code that involves " +
             "handling different cases (in particular error cases)."
         );
